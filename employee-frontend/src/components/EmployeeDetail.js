8c594cc3ba2aa100227162ddfd9949bcef042103
Kisal <kisal@voextechnologies.online>
2025-07-14 21:38:24 +0530

    feat: Add Docker configuration for backend and frontend services with MongoDB integration

--- employee-frontend/src/components/EmployeeDetail.js
+++ employee-frontend/src/components/EmployeeDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function EmployeeDetail(){
     const {id} = useParams();
