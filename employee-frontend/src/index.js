7e4bdf7e8fd8c9b126b2c959d03c927d7b800a53
Kisal <kisal@voextechnologies.online>
2025-08-03 10:46:14 +0530

    refactor: Clean up commented-out code in index.js for better readability

--- employee-frontend/src/index.js
+++ employee-frontend/src/index.js
@@ -1,33 +1,33 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-// import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-import React from "react";
-import ReactDOM from "react-dom/client"; // ✅ use 'client' here
-import App from "./App";
-import keycloak from "./components/keycloak";
+  //   <React.StrictMode>
+  //     <App />
+  //   </React.StrictMode>
+  // );
+  
+  // // If you want to start measuring performance in your app, pass a function
+  // // to log results (for example: reportWebVitals(console.log))
+  // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  // reportWebVitals();
+  
+  import React from "react";
+  import ReactDOM from "react-dom/client"; // ✅ use 'client' here
+  import App from "./App";
+  import keycloak from "./components/keycloak";
+  import './index.css';
+  import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 keycloak.init({ onLoad: "login-required" }).then((authenticated) => {
   if (authenticated) {
-    window.keycloak = keycloak;
+    // window.keycloak = keycloak;
 
     root.render(
       // ✅ use root.render instead of ReactDOM.render
